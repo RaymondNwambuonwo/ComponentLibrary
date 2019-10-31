b55e4fbf8157a561ac9b7b58949cfe14ca067758
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-10-31 16:50:20 -0400

    form started

---
+++ src/components/forms/forms.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './forms.css';
+
+
+const Forms = (props) => (
+    <input style={props.style}>
+        {props.label}
+    </input>
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Forms;
---
+++ src/components/forms/forms.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import forms from './forms';
+
+
+
+
+
+
+storiesOf('forms', module)
+    // .add('Primary', () => <Button label="Primary" />)
