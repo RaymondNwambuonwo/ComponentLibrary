d5c4ccde68fa65b6a3cb2443ea462d0c2026a89d
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-10-31 15:31:53 -0400

    buttons complete

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,13 +4,19 @@ import './Button.css';
 // This is a functional component - just sent up a little differently as an arrow function!
 const Button = (props) => {
     let classList = ''
-    let types = ['primary', 'danger', 'success', 'warning','default','success']
+    let types = ['primary', 'danger', 'success', 'warning','default','success','primary-blend', 'danger-blend', 'success-blend', 'warning-blend', 'default-blend']
     if (types.includes(props.type)) {
         classList += ` button-${props.type}`
     }
     if (props.large) {
         classList += ` button-large`
     }
+    if (props.outline) {
+        classList += ` button-outline`
+        classList += ` button-outline-${props.type}`
+    }
+
+    
    return <button className={classList}>
         {props.label}
     </button>
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -5,13 +5,41 @@ import Button from './Button';
 
 
 storiesOf('Button', module) //where we are naming our group of stories
-    .add('Primary', () => <Button label="Primary" type="primary"/>)
-    .add('Danger', () => <Button label="Danger" type="danger" />)
-    .add('Warning', () => <Button label="Warning" type="warning" />)
-    .add('Default', () => <Button label="Default" type="default" />)
-    .add('Success', () => <Button label="Success" type="success" />)
-    // .add('Large Primary', () => <Button label="Large" type="primary" large/>)
-    // .add('Large Danger', () => <Button label="Large" type="danger" large/>)
-    // .add('Outline Warning', () => <Button label="Outline" type="warning"/>)
+    .add('Primary', () => <Button label="Do Something" type="primary"/>)
+    .add('Danger', () => <Button label="Do Something" type="danger" />)
+    .add('Warning', () => <Button label="Do Something" type="warning" />)
+    .add('Default', () => <Button label="Do Something" type="default" />)
+    .add('Success', () => <Button label="Do Something" type="success" />)
+
+    .add('Outline Primary Button', () => <Button label="Do Something" type="primary" outline/>)
+    .add('Outline Danger Button', () => <Button label="Do Something" type="danger" outline/>)
+    .add('Outline Warning Button', () => <Button label="Do Something" type="warning" outline/>)
+    .add('Outline Default Button', () => <Button label="Do Something" type="default" outline/>)
+    .add('Outline Success Button', () => <Button label="Do Something" type="success" outline/>)
+
+    .add('Primary Blend', () => <Button label="Do Something" type="primary-blend"/>)
+    .add('Danger Blend', () => <Button label="Do Something" type="danger-blend"/>)
+    .add('Warning Blend', () => <Button label="Do Something" type="warning-blend"/>)
+    .add('Default Blend', () => <Button label="Do Something" type="default-blend"/>)
+    .add('Success Blend', () => <Button label="Do Something" type="success-blend"/>)
+
+
+    .add('Large Primary', () => <Button label="Do Something" type="primary" large/>)
+    .add('Large Danger', () => <Button label="Do Something" type="danger" large/>)
+    .add('Large Warning', () => <Button label="Do Something" type="warning" large/>)
+    .add('Large Default', () => <Button label="Do Something" type="default" large/>)
+    .add('Large Success', () => <Button label="Do Something" type="success" large/>)
+
+    .add('Large Outline Primary', () => <Button label="Do Something" type="primary" outline large/>)
+    .add('Large Outline Danger', () => <Button label="Do Something" type="danger" outline large/>)
+    .add('Large Outline Warning', () => <Button label="Do Something" type="warning" outline large/>)
+    .add('Large Outline Default', () => <Button label="Do Something" type="default" outline large/>)
+    .add('Large Outline Success', () => <Button label="Do Something" type="success" outline large/>)
+
+    .add('Large Primary Blend', () => <Button label="Do Something" type="primary-blend"/>)
+    .add('Large Danger Blend', () => <Button label="Do Something" type="danger-blend"/>)
+    .add('Large Warning Blend', () => <Button label="Do Something" type="warning-blend"/>)
+    .add('Large Default Blend', () => <Button label="Do Something" type="default-blend"/>)
+    .add('Large Success Blend', () => <Button label="Do Something" type="success-blend"/>)
     
     //first primary is name of story and second primary is text in button
