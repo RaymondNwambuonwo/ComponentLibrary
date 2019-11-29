ff8b308c95eb7a4ca9b9f32ab38ce51852af9270
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-11-29 16:35:25 -0500

    final fix

--- src/components/Thumbnails/Thumbnails.js
+++ src/components/Thumbnails/Thumbnails.js
@@ -1,4 +1,4 @@
-import React, { Component } from './node_modules/react';
+import React, { Component } from 'react';
 import './Thumbnails.css';
 import macbook from './macbook.svg';
 import star from './star.svg';
